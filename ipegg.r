60d1bb2ffa6c2b72deb4c9651979277a75358bbe
mathiashole <mangino@fcien.edu.uy>
2025-05-26 11:26:37 -0300

    Update itol execution

--- ipegg.r
+++ ipegg.r
@@ -276,4 +276,7 @@ convert_to_itol <- function(data, output_file = "itol_domains.txt", dataset_labe
 }
 
 # # Example usage:
+if(itol_data) {
+  convert_to_itol(myset, output_file = "my_domains.itol", dataset_label = "My Protein Domains")
+}
 # convert_to_itol(myset, output_file = "my_domains.itol", dataset_label = "My Protein Domains")

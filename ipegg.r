ceff25bfaa1c6142b1b9c72146a798e9d4bc7433
Mathias Mangino <94571367+mathiashole@users.noreply.github.com>
2026-04-17 12:33:00 -0300

    Update ipegg.r
    Update message no words remove

--- ipegg.r
+++ ipegg.r
@@ -90,6 +90,8 @@ if (!is.null(words_to_remove) && length(words_to_remove) > 0) {
   df_filtered <- df_sorted %>%
     filter(!grepl(paste(words_to_remove, collapse = "|"),
                   V5, ignore.case = TRUE))
+} else {
+  message("No words specified for removal. Skipping domain filtering.")
 }
 
 # Tree ordering (optional)

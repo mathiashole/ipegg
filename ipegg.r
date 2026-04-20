160e49542e26dd13e65023ba488ccbd9eebad348
mathiashole <mangino@fcien.edu.uy>
2026-04-20 11:38:35 -0300

    Update save svg

--- ipegg.r
+++ ipegg.r
@@ -227,7 +227,9 @@ base_name <- if (!is.null(output_pref)) {
   sub("\\.[^.]*$", "", basename(input_file))
 }
 
-output_svg <- paste0(base_name, ".svg")
+svg(output_svg, width = 18, height = 10)
+
+
 output_png <- paste0(base_name, ".png")
 output_pdf <- paste0(base_name, ".pdf")
 output_itol <- paste0(base_name, ".itol")
